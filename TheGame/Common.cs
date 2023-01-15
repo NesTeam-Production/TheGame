8a1b2b9c8ed190b34a5f0f501d8d4584150f059c
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2023-01-15 16:58:19 +0100

    Add weapon factory & file read/write

---
+++ TheGame/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGame
+{
+    public static class Common
+    {
+        public static string GetWorkingDirectory() => Environment.CurrentDirectory;
+    }
+}
