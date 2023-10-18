d701ba87f1f0b044c7a9689f971e957cfb639998
Szpoti <balage.farago@gmail.com>
2023-10-18 16:48:43 +0200

    Add race option and call GenerateFullCharacter with choosen properties

--- TheGame/Factories/CharacterFactory.cs
+++ TheGame/Factories/CharacterFactory.cs
@@ -1,4 +1,5 @@
 ﻿using TheGame.Classes;
+using TheGame.Races;
 
 namespace TheGame.Factories
 {
@@ -8,12 +9,18 @@ namespace TheGame.Factories
         {
             int level = ChooseLevel();
             Class choosenClass = ChooseClass();
+            Race choosenRace = ChoosenRace();
+
+            Task task = OpenAIHandler.GenerateFullCharacter(level, choosenClass, choosenRace);
+            Console.WriteLine("Waiting for Task to finish...");
+            task.Wait();
+            Console.WriteLine("It's finished!");
         }
 
         private static int ChooseLevel()
         {
             Console.WriteLine("What level are you? (1-20)");
-            var choice = Console.ReadKey().KeyChar.ToString();
+            var choice = Console.ReadLine();
             bool indexCorrect =
                 int.TryParse(choice, out int choiceIndex)
                 && choiceIndex >= 1
@@ -21,7 +28,7 @@ namespace TheGame.Factories
             while (!indexCorrect)
             {
                 Logger.LogRed("Invalid input, try again.");
-                choice = Console.ReadKey().KeyChar.ToString();
+                choice = Console.ReadLine();
                 indexCorrect =
                 int.TryParse(choice, out choiceIndex)
                 && choiceIndex >= 1
@@ -39,7 +46,7 @@ namespace TheGame.Factories
                 Console.WriteLine($"({i}) - {classes.GetValue(i - 1)}");
             }
 
-            var choice = Console.ReadKey().KeyChar.ToString();
+            var choice = Console.ReadLine();
             bool indexCorrect =
                 int.TryParse(choice, out int choiceIndex)
                 && choiceIndex >= 1
@@ -47,13 +54,13 @@ namespace TheGame.Factories
             while (!indexCorrect)
             {
                 Logger.LogRed("Invalid input, try again.");
-                choice = Console.ReadKey().KeyChar.ToString();
+                choice = Console.ReadLine();
                 indexCorrect =
                 int.TryParse(choice, out choiceIndex)
                 && choiceIndex >= 1
                 && choiceIndex <= classes.Length;
             }
-            var choosenClass = classes.GetValue(choiceIndex);
+            var choosenClass = classes.GetValue(choiceIndex - 1);
             return choosenClass switch
             {
                 ClassList.Fighter => new Fighter(),
@@ -61,6 +68,39 @@ namespace TheGame.Factories
                 ClassList.Wizard => new Wizard(),
                 _ => throw new InvalidOperationException(),
             };
+        }
+
+        private static Race ChoosenRace()
+        {
+            {
+                var races = Enum.GetValues(typeof(RaceList));
+                Console.WriteLine("Choose your Race:");
+                for (int i = 1; i <= races.Length; i++)
+                {
+                    Console.WriteLine($"({i}) - {races.GetValue(i - 1)}");
+                }
+
+                var choice = Console.ReadLine();
+                bool indexCorrect =
+                    int.TryParse(choice, out int choiceIndex)
+                    && choiceIndex >= 1
+                    && choiceIndex <= races.Length;
+                while (!indexCorrect)
+                {
+                    Logger.LogRed("Invalid input, try again.");
+                    choice = Console.ReadLine();
+                    indexCorrect =
+                    int.TryParse(choice, out choiceIndex)
+                    && choiceIndex >= 1
+                    && choiceIndex <= races.Length;
+                }
+                var choosenRace = races.GetValue(choiceIndex - 1);
+                return choosenRace switch
+                {
+                    RaceList.Human => new Human(),
+                    _ => throw new InvalidOperationException(),
+                };
+            }
         }
     }
 }
