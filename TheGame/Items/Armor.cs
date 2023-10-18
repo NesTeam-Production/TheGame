ee585bb63dff787f2be18dd5cf20aa402c648d0f
Szpoti <balage.farago@gmail.com>
2023-10-18 16:48:57 +0200

    Add Armor.cs

---
+++ TheGame/Items/Armor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGame.Items
+{
+    public class Armor : Item
+    {
+        public Armor(int id, string name, int armorClass, string description = "") : base(id, name, description)
+        {
+            ArmorClass = armorClass;
+        }
+
+        public int ArmorClass { get; private set; }
+    }
+}
